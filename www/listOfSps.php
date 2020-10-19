2da290d4f333911a87435890a98020f043c6b864
Pavel Břoušek <melanger@users.noreply.github.com>
2020-10-19 19:47:31 +0200

    Take service name in listOfSps from an attribute

--- www/listOfSps.php
+++ www/listOfSps.php
@@ -11,6 +11,7 @@ const PROXY_IDENTIFIER = 'listOfSps.proxyIdentifier';
 const ATTRIBUTES_DEFINITIONS = 'listOfSps.attributesDefinitions';
 const SHOW_OIDC_SERVICES = 'listOfSps.showOIDCServices';
 
+const PERUN_SERVICE_NAME_ATTR_NAME = 'listOfSps.serviceNameAttr';
 const PERUN_PROXY_IDENTIFIER_ATTR_NAME = 'listOfSps.perunProxyIdentifierAttr';
 const PERUN_LOGIN_URL_ATTR_NAME = 'listOfSps.loginURLAttr';
 const PERUN_TEST_SP_ATTR_NAME = 'listOfSps.isTestSpAttr';
@@ -62,6 +63,7 @@ if ($showOIDCServices && empty($perunOidcClientIdAttr)) {
     );
 }
 
+$perunServiceNameAttr = $conf->getString(PERUN_SERVICE_NAME_ATTR_NAME, 'urn:perun:facility:attribute-def:core:name');
 $perunLoginURLAttr = $conf->getString(PERUN_LOGIN_URL_ATTR_NAME, null);
 $perunTestSpAttr = $conf->getString(PERUN_TEST_SP_ATTR_NAME, null);
 $perunShowOnServiceListAttr
@@ -76,6 +78,7 @@ $facilities
 $attrNames = [];
 
 array_push($attrNames, $perunSaml2EntityIdAttr);
+array_push($attrNames, $perunServiceNameAttr);
 if (!empty($perunOidcClientIdAttr)) {
     array_push($attrNames, $perunOidcClientIdAttr);
 }
@@ -161,7 +164,7 @@ if (isset($_GET['output']) && $_GET['output'] === 'json') {
     $json['statistics']['oidcTestServicesCount'] = $statistics['oidcTestServicesCount'];
     foreach ($allServices as $service) {
         $a = [];
-        $a['name'] = $service['facility']->getName();
+        $a['name'] = $service['facilityAttributes'][$perunServiceNameAttr]['value'];
 
         if (array_key_exists($service['facility']->getID(), $samlServices)) {
             $a['authenticationProtocol'] = 'SAML';
