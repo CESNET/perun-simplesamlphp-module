61ee3a64acd527ea2507f76f11df5c7377f95fb7
Pavel Vyskočil <vyskocilpavel@muni.cz>
2020-10-23 15:15:39 +0200

    Fix List of SPs
    * 'listOfSps.perunProxyIdentifierAttr' should be internal name

--- lib/ListOfSps.php
+++ lib/ListOfSps.php
@@ -5,7 +5,7 @@ class ListOfSps
 {
     public static function sortByName($a, $b)
     {
-        return strcmp(strtolower($a['facility']->getName()), strtolower($b['facility']->getName()));
+        return strcmp(strtolower($a['name']['value']), strtolower($b['name']['value']));
     }
 
     public static function getClass($type)
