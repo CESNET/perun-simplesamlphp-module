49d8ee0f9cdaf047b4c702d941129ee3ef3b8c69
Dominik František Bučík <bucik@ics.muni.cz>
2022-05-19 11:21:57 +0200

    fix: 🐛 Fix templates perun-aup and perun-user, update dicts

--- templates/perun-user-tpl.php
+++ templates/perun-user-tpl.php
@@ -2,14 +2,16 @@
 
 use SimpleSAML\Module\perun\Auth\Process\PerunUser;
 
+$this->data['header'] = $this->t('{perun:perun:register_header}');
+
 $this->includeAtTemplateBase('includes/header.php');
 
 ?>
 
 <div class="row">
-    <div class="offset-1 col-10 offset-sm-1 col-sm-10 offset-md-2 col-md-8 offset-lg-3 col-lg-6 offset-xl-3 col-xl-6">
+    <div>
         <p><?php echo $this->t('{perun:perun:register_text}'); ?></p>
-        <a class="btn btn-block" href="<?php echo $this->data[PerunUser::PARAM_REGISTRATION_URL]; ?>">
+        <a class="btn btn-block btn-primary" href="<?php echo $this->data[PerunUser::PARAM_REGISTRATION_URL]; ?>">
             <?php echo $this->t('{perun:perun:register_button}'); ?>
         </a>
     </div>
